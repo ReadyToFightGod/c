174acdb25f081db9469c33760a179cf9ea2c8a92
Kate <81093059+ReadyToFightGod@users.noreply.github.com>
2021-09-23 15:48:00 +0300

    Add files via upload

---
+++ homework3/hw3ex4.cpp
@@ -0,0 +1,20 @@
+#include <functional>
+#include <vector>
+#include <iostream>
+int main(){
+    std::vector<int> s = {2, 6, 8,0, 15, 31, -5};
+    std::sort(s.begin(), s.end(), [](int a, int b) {
+        return a > b;
+    });
+    for(int i = 0; i < 7; i++){
+        std::cout << s[i] << std::endl;
+    }
+    std::sort(s.begin(), s.end(), [](int a, int b) {
+        return a < b;
+    });
+    for(int i = 0; i < 7; i++){
+        std::cout << s[i] << std::endl;
+    }
+}
+
+
