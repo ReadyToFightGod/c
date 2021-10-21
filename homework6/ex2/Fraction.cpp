2df080ff68dd507508778ea3bca1013e8cda061c
Kate <81093059+ReadyToFightGod@users.noreply.github.com>
2021-10-21 14:52:29 +0300

    Update Fraction.cpp

--- homework6/ex2/Fraction.cpp
+++ homework6/ex2/Fraction.cpp
@@ -38,25 +38,44 @@ Fraction operator/(const Fraction &f1, const Fraction &f2){
     return res;
 }
 //перегрузка префиксного
-Fraction operator++(const Fraction &f1){
-    auto res = Fraction(f1.num + f1.num, f1.den);
-    res.Reduce();
-    return res;
+Fraction& Fraction::operator++(){
+    this->num += den;
+    (*this).Reduce();
+    return *this;
 }
 //перегрузка префиксного
-Fraction operator--(const Fraction &f1){
-    auto res = Fraction(f1.num - f1.num, f1.den);
-    res.Reduce();
-    return res;
+Fraction& Fraction::operator--(){
+    this->num -= den;
+    (*this).Reduce();
+    return *this;
+}
+//перегрузка постфиксного
+Fraction Fraction::operator++(int){
+    Fraction tmp(num, den);
+    ++(*this);
+    return tmp;
+}
+//перегрузка постфикстного
+Fraction Fraction::operator--(int){
+    Fraction tmp(num, den);
+    --(*this);
+    return tmp;
 }
 bool operator<(const Fraction &f1, const Fraction &f2){
-    if(f1.num * f2.den < f1.den * f2.num){return true;}
-    else{return false;}
-};
+    return f1.num * f2.den < f1.den * f2.num;
+}
 bool operator>(const Fraction &f1, const Fraction &f2){
-    if(f1.num * f2.den > f1.den * f2.num){return true;}
-    else{return false;}
-};
+    return f1.num * f2.den > f1.den * f2.num;
+}
+bool operator==(const Fraction &f1, const Fraction &f2){
+    return f1.num * f2.den == f1.den * f2.num;
+}
+bool operator<=(const Fraction &f1, const Fraction &f2){
+    return f1.num * f2.den <= f1.den * f2.num;
+}
+bool operator>=(const Fraction &f1, const Fraction &f2){
+    return f1.num * f2.den >= f1.den * f2.num;
+}
 ostream& operator<<(ostream& ostr, const Fraction& src){
     return cout << src.get_numerator() << "/" << src.get_denominator();
 }
