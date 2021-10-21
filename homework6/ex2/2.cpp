33c45084c7b29db740e94a250fd7937ca6290228
Kate <81093059+ReadyToFightGod@users.noreply.github.com>
2021-10-21 14:51:51 +0300

    Update 2.cpp

--- homework6/ex2/2.cpp
+++ homework6/ex2/2.cpp
@@ -3,7 +3,7 @@
 int main( ){
     Fraction f1, f2; char op;
     cout << "Enter first fraction (like a/b)\n"; cin >> f1;
-    cout << "Enter operator (+, -, *, /, ++, --) \n"; cin >> op;
+    cout << "Enter operator (+, -, *, /, >, <) \n"; cin >> op;
     cout << "Enter second fraction (like a/b)\n"; cin >> f2;
     switch (op){
         case'+':
@@ -22,8 +22,8 @@ int main( ){
             cout << "Error!";
             break;
     }
-    cout << "++" << f1 << ++f1 << endl;
-    cout << "--" << f1 << --f1 << endl;
+    cout << "++" << f1 << ++f1 << "; "<< f1 << "++" << f1++ << endl;
+    cout << "--" << f1 << --f1 << "; "<< f1 << "--" << f1-- << endl;
     cout << f1 << " in double is " << double(f1) << endl;
     cout << f2 << " in double is " << double(f2) << endl;
 }
