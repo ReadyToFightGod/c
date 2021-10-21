e8c2004a81e80ffac31037b0f6283d92b17c0b59
Kate <81093059+ReadyToFightGod@users.noreply.github.com>
2021-10-21 14:52:58 +0300

    Update Fraction.h

--- homework6/ex2/Fraction.h
+++ homework6/ex2/Fraction.h
@@ -5,30 +5,25 @@ using namespace std;
 
 class Fraction{
 private:
-    signed int num; int den;
+    int num; unsigned int den;
 public:
-    explicit Fraction(signed int a = 0, int b = 0): num(a), den(b) {}
+    explicit Fraction(int a = 0, unsigned int b = 0): num(a), den(b) {}
 
     Fraction(const Fraction& src): num(src.num), den(src.den){}
 
-    signed int get_numerator()  const {
+    int get_numerator()  const {
         return num;
     }
-    int get_denominator() const {
+    unsigned int get_denominator() const {
         return den;
     }
     int gcd(int a, int b) {}
     void Reduce(){}
     Fraction& operator=(const Fraction& src ){}
-    //перегрузка постфиксного
-//    Fraction operator++(){
-//        this->num += den;
-//        return *this;
-//    }
-//    Fraction operator--(){
-//        this->num -= den;
-//        return *this;
-//    }
+    Fraction operator++(int); //перегрузка постфиксного
+    Fraction operator--(int); //перегрузка ппостфиксного
+    Fraction& operator++();//перегрузка префиксного
+    Fraction& operator--();//перегрузка префиксного
     explicit operator double() const {
         return double(num)/double(den);
     }
@@ -40,19 +35,19 @@ public:
     friend Fraction operator/(const Fraction &f1, const Fraction &f2);
     friend bool operator<(const Fraction &f1, const Fraction &f2);
     friend bool operator>(const Fraction &f1, const Fraction &f2);
-    friend Fraction operator++(const Fraction &f1);
-    friend Fraction operator--(const Fraction &f1);
+    friend bool operator>=(const Fraction &f1, const Fraction &f2){};
+    friend bool operator<=(const Fraction &f1, const Fraction &f2){};
+    friend bool operator==(const Fraction &f1, const Fraction &f2){};
 };
 Fraction operator+(const Fraction &f1, const Fraction &f2){}
 Fraction operator-(const Fraction &f1, const Fraction &f2){}
 Fraction operator*(const Fraction &f1, const Fraction &f2){}
 Fraction operator/(const Fraction &f1, const Fraction &f2){}
-//перегрузка префиксного
-Fraction operator++(const Fraction &f1){}
-//перегрузка префиксного
-Fraction operator--(const Fraction &f1){}
-bool operator<(const Fraction &f1, const Fraction &f2){};
-bool operator>(const Fraction &f1, const Fraction &f2){};
+bool operator<(const Fraction &f1, const Fraction &f2){}
+bool operator>(const Fraction &f1, const Fraction &f2){}
+bool operator>=(const Fraction &f1, const Fraction &f2){}
+bool operator<=(const Fraction &f1, const Fraction &f2){}
+bool operator==(const Fraction &f1, const Fraction &f2){}
 ostream& operator<<(ostream& ostr, const Fraction& src){}
 istream& operator>>(istream& in, Fraction& f){}
 #endif //INC_2_CPP_FRACTION_H
