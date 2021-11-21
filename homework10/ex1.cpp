817b6a4682f434a3e4125c251d70e66a803a4655
Kate <81093059+ReadyToFightGod@users.noreply.github.com>
2021-11-21 22:17:30 +0300

    Update ex1.cpp

--- homework10/ex1.cpp
+++ homework10/ex1.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
-auto f = [](auto x){std::cout << x << std::endl; return x + 5; };
-template<typename F>
-void test(F f){
-    auto a = f(32);
-    auto b = f(32.77);
-    auto c = f(2000000000ull);
-    std::cout << a << ' ' << b << ' ' << c << std::endl;
+auto f = [](auto... x){return (x + ...); };
+template<typename F, typename... Types>
+void test(F f, Types... x){
+    std::cout << f(x...);
 }
 int main(){
-    test(f);
+    test(f, 0, 8, 9);
 }
 
