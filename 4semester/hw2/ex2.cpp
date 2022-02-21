3ae3c72bb07505854b00a60aa1bde2f8ca5e2869
Kate <81093059+ReadyToFightGod@users.noreply.github.com>
2022-02-21 16:41:21 +0300

    Update ex2.cpp

--- 4semester/hw2/ex2.cpp
+++ 4semester/hw2/ex2.cpp
@@ -1,49 +1,51 @@
-//Разработайте контейнер для хранения телефонного справочника. Согласно контракту, необходимо обеспечить:
-//Доступ к записям, отсортированным по фамилии человека (для городской типографии).
-//Произвольный доступ к записям справочника (для рекламного агентства).
-//Быстрый поиск по фамилии человека (для регулярных пользователей).
-
 #include <iostream>
 #include <map>
-#include <vector>
 #include <algorithm>
 #include <chrono>
 #include <random>
 
-void typography(std::string s, std::map<std::string, int> m){
-    for(auto it = m.begin(); it != m.end(); it++){
-        if (it->first == s){
-            std::cout << s << " : " << it->second << std::endl;
-            return;
-        }
-    }
-    std::cout << "Error" << std::endl;
-}
+class Container{
+private:
+    std::map<std::string, int> cont;
+public:
+    Container(const std::map<std::string, int> &cont) : cont(cont) {}
 
-void quick(std::string s, std::map<std::string, int> m){
-    std::cout << s << " : " << m.find(s)->second << std::endl;
-}
-
-void agency(std::map<std::string,int> m){
-    int e = m.size(); auto k = 0;
-    auto current_timestamp = std::chrono::steady_clock::now().time_since_epoch().count();
-    auto generator = std::mt19937_64(current_timestamp);
-    auto distribution = std::uniform_int_distribution(0, e-1);
-    int c = distribution(generator);
-    for(auto it = m.begin(); it != m.end(); it++){
-        if (c != k) {k++; continue;}
-        else {
-            std::cout << it->first << " : " << it->second << std::endl;
-            return;
+    void typography(std::string s){
+        for(auto it = cont.begin(); it != cont.end(); it++){
+            if (it->first == s){
+                std::cout << s << " : " << it->second << std::endl;
+                return;
+            }
         }
+        std::cout << "Error" << std::endl;
     }
-    return;
-}
+
+    void quick(std::string s){
+        std::cout << s << " : " << cont.find(s)->second << std::endl;
+    }
+
+    void agency(){
+        int e = cont.size(); auto k = 0;
+        auto current_timestamp = std::chrono::steady_clock::now().time_since_epoch().count();
+        auto generator = std::mt19937_64(current_timestamp);
+        auto distribution = std::uniform_int_distribution(0, e-1);
+        int c = distribution(generator);
+        for(auto it = cont.begin(); it != cont.end(); it++){
+            if (c != k) {k++; continue;}
+            else {
+                std::cout << it->first << " : " << it->second << std::endl;
+                return;
+            }
+        }
+        return;
+    }
+};
+
 int main() {
-    std::map<std::string, int> s{{"Me",  1}, {"dude",  4104},
-                                 {"Niki", 588434}, {"nice",  42069},
-                                 {"Max",  2}, {"sEIGHTan", 666}};
-    typography("Max", s);
-    quick("sEIGHTan", s);
-    agency(s);
+    Container s({{"Me",  1}, {"dude",  4104},
+                  {"Niki", 588434}, {"nice",  42069},
+                  {"Max",  2}, {"sEIGHTan", 666}});
+    s.typography("Max");
+    s.quick("sEIGHTan");
+    s.agency();
 };
